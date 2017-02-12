d9bb386c45a7b7cb96ff5e4c5fe905a258a8ea5b
Mengzhen <gao.mengzhen94@gmail.com>
2017-02-12 00:41:15 -0500

    display results using angular-chart

--- app/controllers/eachpoll.client.js
+++ app/controllers/eachpoll.client.js
@@ -1,12 +1,23 @@
 'use strict';
 (function(){
-   angular.module("mypollApp", []).controller("eachpoll", ["$scope", "$http","$parse",  function($scope, $http, $parse){
+   angular.module("mypollApp",["chart.js"])
+   .config(["ChartJsProvider", function (ChartJsProvider) {
+        
+        ChartJsProvider.setOptions({
+            responsive: true,
+            animationEasing: "easeOutQuart",
+            animationSteps: 40
+        });
+        
+}]).controller("eachpoll", ["$scope", "$http","$parse",  function($scope, $http, $parse){
 
-		var thisId = window.location.pathname.slice(7);
-   		var url = "/showpolls/" + thisId;
-   		//console.log("thisUrl: ",url);
+        var thisId = window.location.pathname.slice(7);
+        var url = "/showpolls/" + thisId;
+        //console.log("thisUrl: ",url);
 
-   		$scope.options = []; 
+        $scope.options = []; 
+        $scope.labels = []; 
+        $scope.data = []; 
 
         $http.get(url, {cache: true}).then(function(res){
 
@@ -14,15 +25,15 @@
             $scope.id = res.data["_id"];
             //$scope.url = '/polls/' + res.data["_id"].toString();
             res.data.options.forEach(function(option){
-            	$scope.options.push({
-                	"op": option.text,
+                 $scope.labels.push(option.text);
+                 $scope.data.push(option.votes);
+                $scope.options.push({
+                    "op": option.text,
                   "num": option.votes
-            	});
+                });
             });
-
-
 
         },function(err){throw err;});
         
-  	}]); 
+    }]); 
 })();
