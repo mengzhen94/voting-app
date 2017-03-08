83ab54caff419c18f6705551f5dd36c9c7649baf
Mengzhen <gao.mengzhen94@gmail.com>
2017-03-08 16:13:26 -0500

    fix ip

--- app/config/auth.js
+++ app/config/auth.js
@@ -1,17 +1,17 @@
 'use strict';
-var port = process.env.PORT || 8080;
+//var port = process.env.PORT || 8080;
 var ip = process.env.IP || process.env.localIP;
 
 module.exports = {
 	'githubAuth':{
 		'clientID': process.env.GITHUB_KEY,
 		'clientSecret': process.env.GITHUB_SECRET,
-		'callbackURL': ip + ':' + port + '/auth/github/callback'
+		'callbackURL': ip + '/auth/github/callback'
 	},
 
 	'facebookAuth': {
         'clientID': process.env.FACE_KEY,
         'clientSecret': process.env.FACE_SECRET,
-        'callbackURL': ip + ':' + port + '/auth/facebook/callback'
+        'callbackURL': ip + '/auth/facebook/callback'
     }
 };
