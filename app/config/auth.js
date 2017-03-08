4363042d688c7c54daf56618dfd6c2b39f87d736
Mengzhen <gao.mengzhen94@gmail.com>
2017-03-08 16:00:11 -0500

    update auth.js for diploy

--- app/config/auth.js
+++ app/config/auth.js
@@ -1,15 +1,17 @@
 'use strict';
+var port = process.env.PORT || 8080;
+var ip = process.env.IP || process.env.localIP;
 
 module.exports = {
 	'githubAuth':{
 		'clientID': process.env.GITHUB_KEY,
 		'clientSecret': process.env.GITHUB_SECRET,
-		'callbackURL': process.env.APP_URL + '/auth/github/callback'
+		'callbackURL': ip + ':' + port + '/auth/github/callback'
 	},
 
 	'facebookAuth': {
         'clientID': process.env.FACE_KEY,
         'clientSecret': process.env.FACE_SECRET,
-        'callbackURL': process.env.APP_URL + '/auth/facebook/callback'
+        'callbackURL': ip + ':' + port + '/auth/facebook/callback'
     }
 };
